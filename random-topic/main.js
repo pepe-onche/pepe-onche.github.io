8fcfa3911bfac1f98c7d1b991130c2afdaf7c707
Pepe <173433735+pepe-onche@users.noreply.github.com>
2024-08-08 08:46:29 +0200

    abcdef

--- random-topic/main.js
+++ random-topic/main.js
@@ -1,10 +1,33 @@
 var max = 600000;
 
-function redirect() {
-  window.location.replace('https://onche.org/topic/'+Math.floor(Math.random()*max+1))
+async function redirect() {
+  window.location.replace(await getRandomTopicURL())
 }
 
-function newTab() {
-  var win = window.open('https://onche.org/topic/'+Math.floor(Math.random()*max+1), '_blank');
+async function newTab() {
+  var win = window.open(await getRandomTopicURL(), '_blank');
   win.focus();
+}
+
+async function getRandomTopicURL() {
+  let done = false
+  let topicId = null
+  let url = null
+  let tries = 0
+
+  while (!done) {
+    if (tries >= 8) throw Error()
+
+    topicId = Math.floor(Math.random()*max+1)
+    url = 'https://onche.org/topic/'+topicId
+    const res = await fetch(url)
+
+    if (res.ok) {
+      done = true
+    }
+
+    tries += 1
+  }
+
+  return url
 }
