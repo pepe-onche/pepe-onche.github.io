cf79c06b96149052fbf1b4e005dc6cf7ebbdb86b
Pepe <173433735+pepe-onche@users.noreply.github.com>
2025-03-12 23:46:36 +0100

    increase event timeout

--- tamagonche/game.js
+++ tamagonche/game.js
@@ -122,7 +122,7 @@ const channel = sb
             document.getElementById('events').appendChild(div);
             setTimeout(() => {
                 div.remove();
-            }, 6000);
+            }, 30000);
         }
     }
   )
